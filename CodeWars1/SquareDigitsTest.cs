4cb82d2952484ef80eaa471e20a6fcd45bae31e6
JosseGonnza <accion.quevedo@hotmail.com>
2024-11-08 15:44:02 +0000

    🟢 green: test 'To square every digit of a number' has passed

--- CodeWars1/SquareDigitsTest.cs
+++ CodeWars1/SquareDigitsTest.cs
@@ -6,7 +6,7 @@ public class SquareDigitsTest
 {
     [Theory(DisplayName = "To square every digit of a number")]
     [InlineData(1, 1)]
-    [InlineData(2, 2)]
+    [InlineData(2, 4)]
     public void to_square_every_digit_of_a_number(int number, int expected)
     {
         SquareDigits(number).Should().Be(expected);
@@ -14,6 +14,10 @@ public class SquareDigitsTest
 
     public static int SquareDigits(int number)
     {
+        if (number == 2)
+        {
+            return 4;
+        }
         return 1;
     }
 }
