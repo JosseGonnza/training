54c293d774cd889d6d2f73b856bf4ae438bee5b5
JosseGonnza <accion.quevedo@hotmail.com>
2024-11-09 11:56:31 +0000

    ✨ feat: Ex 1.2.1

--- RoadmapGPT.Day1/Program.cs
+++ RoadmapGPT.Day1/Program.cs
@@ -4,5 +4,13 @@
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Jose Gonzalez!");
+
+        int age = 28;
+        double money = 150.25;
+        string name = "Jose Gonzalez";
+        
+        Console.WriteLine(age);
+        Console.WriteLine(money);
+        Console.WriteLine(name);
     }
 }
