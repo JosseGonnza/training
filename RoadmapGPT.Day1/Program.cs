3d7a6a5026a0e6fa87c117692e3da329a4bc37a2
JosseGonnza <accion.quevedo@hotmail.com>
2024-11-09 12:10:21 +0000

    ✨ feat: Ex 2.1

--- RoadmapGPT.Day1/Program.cs
+++ RoadmapGPT.Day1/Program.cs
@@ -2,15 +2,21 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Jose Gonzalez!");
-
-        int age = 28;
-        double money = 150.25;
-        string name = "Jose Gonzalez";
+        Console.WriteLine("Introduce el primer numero");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Introduce el segundo numero");
+        int number2 = Convert.ToInt32(Console.ReadLine());
         
-        Console.WriteLine(age);
-        Console.WriteLine(money);
-        Console.WriteLine(name);
+        Console.WriteLine("\n");
+        
+        var sum = number1 + number2;
+        var rest = number1 - number2;
+        var product = number1 * number2;
+        var division = ((double)number1 / number2);
+        
+        Console.WriteLine("La suma de los numeros es: " + sum);
+        Console.WriteLine("La resta de los numeros es: " + rest);
+        Console.WriteLine("La multiplicacion de los numeros es: " + product);
+        Console.WriteLine("La division de los numeros es: " + division);
     }
 }
