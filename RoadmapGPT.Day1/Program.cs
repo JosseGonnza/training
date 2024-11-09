c521acddada95cf38571b6151c0abd7a62939548
JosseGonnza <accion.quevedo@hotmail.com>
2024-11-09 18:49:22 +0000

    ✨ feat: Day 3, Ex 2.1

--- RoadmapGPT.Day1/Program.cs
+++ RoadmapGPT.Day1/Program.cs
@@ -4,17 +4,21 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        var phrase = "Anita lava la tina";
-        Console.WriteLine(IsPalindrome(phrase));
+        int[,] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        Console.WriteLine(SumDiagonals(matriz));
     }
 
-    private static bool IsPalindrome(string phrase)
+    private static int SumDiagonals(int[,] matriz)
     {
-        var newPhrase = phrase.ToLower().Replace(" ", "");
-        var charArray = newPhrase.ToCharArray();
-        Array.Reverse(charArray);
-        var phraseReverse = new string(charArray);
+        int sum = 0;
+        int longitude = matriz.GetLength(0);
 
-        return newPhrase == phraseReverse;
+        for (int i = 0; i < longitude; i++)
+        {
+            sum += matriz[i, i];
+            sum += matriz[i, (longitude - i - 1)];
+        }
+
+        return sum;
     }
 }
